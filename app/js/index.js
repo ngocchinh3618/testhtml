d37ff54ae4dcd52e87d832738bbc756cf9d919d4
ngocchinh3618 <ngocchinh3618@gmail.com>
2021-06-27 17:59:41 +0700

    modal, custom checkbox, animation scroll top, carousel thumbnail, autocomplet, count0-text..

--- app/js/index.js
+++ app/js/index.js
@@ -1,23 +1,79 @@
-//scroll-to-top
-//Get the button:
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        // mybutton.style.display = "block";
-        document.getElementById('myBtn').style.display = 'block'
+$(document).ready(function() {
+    // show hide checkbox detail
+    var checked = $('#attachment-1').prop('checked')
+    if (!checked) {
+        $('#attachment-detail-1').hide();
     } else {
-        document.getElementById('myBtn').style.display = 'none'
+        $('#attachment-detail-1').show();
     }
-}
+    $("#attachment-1").change(function() {
+        if (this.checked) {
+            $('#attachment-detail-1').show();
+        } else {
+            $('#attachment-detail-1').hide();
+        }
+    });
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-    document.documentElement
-}
-//scroll-to-top
+    var checked2 = $('attachment-2').prop('cheked')
+    if (!checked2) {
+        $('#attachment-detail-2').hide();
+    } else {
+        $('#attachment-detail-2').show();
+    }
+    $("#attachment-2").change(function() {
+            if (this.checked) {
+                $('#attachment-detail-2').show();
+            } else {
+                $('#attachment-detail-2').hide();
+            }
+        })
+        // ~show hide checkbox detail
+        // ontop amination
+    $("#myBtn").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 200, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+    //~ ontop amination
+    // count text
+    $('#fname').keyup(function(e) {
+        $('#fname-count').html(this.value.length);
+    }).keypress(function(e) {
+        if (this.value.length >= 10) {
+            e.preventDefault();
+        }
+    });
+    $('#fname2').keyup(function(e) {
+        $('#fname2-count').html(this.value.length);
+    }).keypress(function(e) {
+        if (this.value.length >= 10) {
+            e.preventDefault();
+        }
+    });
+    //~ count text
+
+    // note content
+    // $('.note__content > p')
+    var status = true;
+    $('.note__link').click(function(e) {
+        if (status) {
+            $('.note__content > p').html("<p><b>Special package</b> includes tilting SF bucket + general purpose bucket + draw pin coupler. Some placeholder the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger. </p>")
+            $('.note__link').html("Learn more 🠕 ")
+        } else {
+            $('.note__content > p').html("<p><b>Special package</b> includes tilting SF bucket + general purpose bucket + draw pin coupler. Some placeholder... </p>")
+            $('.note__link').html("Learn more 🠗 ")
+        }
+        status = !status;
+
+    });
+    //~ note content
+    // modal
+    $("#datepicker").datepicker();
+    //~ modal
+});
